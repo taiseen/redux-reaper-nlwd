1208668106722ce7700259080884040162877086
taiseen <taiseen.cse@gmail.com>
2023-08-24 17:11:42 +0600

    feat: UI template add

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import { useAppDispatch, useAppSelector } from "./redux/hook";
-import {
-  incrementByAmount,
-  increment,
-  decrement,
-} from "./redux/feature/counterSlice";
+import { Toaster } from './components/ui/Toaster';
+import MainLayout from './layouts/MainLayout';
 
-const App = () => {
-  const { value } = useAppSelector((state) => state.counter);
-  const dispatch = useAppDispatch();
-
+function App() {
   return (
-    <div className="flex gap-4 justify-center items-center">
-      <button
-        className="p-4 border border-green-400 outline-none cursor-pointer"
-        onClick={() => dispatch(increment())}
-      >
-        +
-      </button>
-      <button
-        className="p-4 border border-green-400 outline-none cursor-pointer"
-        onClick={() => dispatch(incrementByAmount(5))}
-      >
-        +5
-      </button>
-      <h1 className="text-3xl font-bold">{value}</h1>
-      <button
-        className="p-4 border border-red-400 outline-none cursor-pointer"
-        onClick={() => dispatch(decrement())}
-      >
-        -
-      </button>
+    <div>
+      <Toaster />
+      <MainLayout />
     </div>
   );
-};
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom/client";
-import store from "./redux/store";
-import routes from "./routes";
-import React from "react";
-import "./style/index.css";
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import routes from './routes/routes.tsx';
+import ReactDOM from 'react-dom/client';
+import store from './redux/store';
+import React from 'react';
+import './style/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={routes} />
